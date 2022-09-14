dc9e56b423768a041dd4643865fe3e82655a3348
Your Name <you@example.com>
2022-09-14 14:48:49 +0200

    senaste commiten

--- Adventure_Game/Program.cs
+++ Adventure_Game/Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+            //Lite backstory/ inledning på spelet
             Console.WriteLine("Du är en hjälte som fått uppdrag att döda en drake i en skog vid namn Nargusskogar...");
             Console.WriteLine("När du väl vandrar dit så är allt lugnt med fin väg på vägen dit...");
             Console.WriteLine("När du väl är framme så ser skogen ut att vara väldigt tät och mörk...");
