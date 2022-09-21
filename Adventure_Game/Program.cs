536239e18d501e323d30d7f66895bc8f69f944dc
Your Name <you@example.com>
2022-09-21 14:23:35 +0200

    senaste

--- Adventure_Game/Program.cs
+++ Adventure_Game/Program.cs
@@ -4,11 +4,40 @@
     {
         static void Main(string[] args)
         {
+            Start();
+            
+        }
+
+        static void Skogen() {
+            Console.WriteLine("Du står nu djupt inuti skogen och hör något som prasslar");
+            Console.WriteLine("du blir överfallen av ett par vargar.");
+            Console.WriteLine("Hur vill du göra?,vill du attackera eller fly?");
+
+        }
+
+        static void Start() {
+
             //Lite backstory/ inledning på spelet
             Console.WriteLine("Du är en hjälte som fått uppdrag att döda en drake i en skog vid namn Nargusskogar...");
             Console.WriteLine("När du väl vandrar dit så är allt lugnt med fin väg på vägen dit...");
             Console.WriteLine("När du väl är framme så ser skogen ut att vara väldigt tät och mörk...");
             Console.WriteLine("Du blir spänd av skogens mörka känsla men går ändå in...");
+
+            Console.WriteLine("Du står nu vid ingången av skogen, vad vill du göra?");
+            Console.WriteLine("Vill du fortsätta inåt i skogen eller vill du stå kvar?");
+
+            string gå_Framåt = Console.ReadLine();
+
+            if (gå_Framåt == "gå framåt")
+            {
+                Skogen();
+            }
+
+            if (gå_Framåt == "stå kvar")
+            {
+                Start();
+            }
+
 
 
         }
