e2665a05289f6fe48d6bafca248b0a7774c8ee5f
Adrians pc <adrian.stude@gmail.com>
2022-10-09 13:56:03 +0200

    test

--- Adventure_Game/Player.cs
+++ Adventure_Game/Player.cs
@@ -5,13 +5,13 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Adventure_Game
-{
+{   //karaktären som spelaren spelar samt values för motståndare, orkar inte göra separat kanske fixar senare
     class Player
     {
         public int health = 10;
         public int dmg = 1;
-        public  int potion = 5;
-        public  int power = 0;
+        public int potion = 5;
+        public int power = 0;
         public int armorValue = 0;
         public int weaponValue = 1;
     }
